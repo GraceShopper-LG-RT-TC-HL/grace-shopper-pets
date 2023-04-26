0445cbeed0834bf522087f5a732930395eaeac28
Lateisha G <frogstern@gmail.com>
2023-04-26 16:05:22 -0400

    added ability to create an account

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const init = async () => {
     await syncAndSeed();
     const port = process.env.PORT || 3000;
     app.listen(port, () => console.log(`listening on port ${port}`));
-    console.log('create account');
   } catch (ex) {
     console.log(ex);
   }
