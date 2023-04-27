89e66ad376cd3019e0b7f575231cdcbe0e93ce9a
Richard Tang <richardtang27@gmail.com>
2023-04-27 16:47:26 -0700

    1

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express.Router();
-const { User } = require("../db");
+const { User, Order} = require("../db");
 
 module.exports = app;
 
@@ -36,6 +36,23 @@ app.put("/cart", async (req, res, next) => {
     const user = await User.findByToken(req.headers.authorization);
     res.send(await user.removeFromCart(req.body));
   } catch (ex) {
+    next(ex);
+  }
+});
+
+//route to get past orders
+app.get('/', async(req, res, next) => {
+  try{
+    const user = await User.findByToken(req.headers.authorization);
+    const orders = await Order.findAll({
+      
+      where: {
+        userId: user.id,
+        isCart: false
+      }}
+      );
+  }
+  catch(ex){
     next(ex);
   }
 });
