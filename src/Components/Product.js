a816e5a7a54efc931d56ac5515c84f5361f5d2d7
Lateisha G <frogstern@gmail.com>
2023-05-04 16:27:38 -0400

    commit

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Typography } from '@mui/material';
 import Reviews from './Reviews';
 
 const Product = () => {
   const { products } = useSelector((state) => state);
   const { id } = useParams();
   const product = products.find((product) => product.id === id);
-  
-  if(!product){
+
+  if (!product) {
     return null;
   }
   return (
     <div>
-      <h2>{ product.name }</h2>
-      <h3>${ product.price }</h3>
-      <h4>{ product.description }</h4>
-      <img src={ product.imgUrl } />
+      <Typography variant='h4'>{product.name}</Typography>
+      <h3>${product.price}</h3>
+      <h4>{product.description}</h4>
+      <img src={product.imgUrl} />
       <br />
-      
-      <Reviews 
-        product={ product }
-      />
+
+      <Reviews product={product} />
     </div>
-    );
+  );
 };
 
 export default Product;
