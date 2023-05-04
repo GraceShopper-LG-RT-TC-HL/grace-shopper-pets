9dd603ac72fb94b8d499add64ff68338c29bcdac
Lateisha G <frogstern@gmail.com>
2023-05-04 19:55:08 -0400

    improved Addreview, and fixed typo

--- src/Components/AddReview.js
+++ src/Components/AddReview.js
@@ -3,12 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 import { createReview } from '../store';
 import { useNavigate } from 'react-router-dom';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import { Rating, Typography, TextField, Button } from '@mui/material/';
 
 const AddReview = () => {
   const { products, auth } = useSelector((state) => state);
-  const { id } = useParams(); 
+  const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -17,7 +16,7 @@ const AddReview = () => {
   const [rating, setRating] = useState(0);
 
   const product = products.find((p) => p.id === id);
-  
+
   if (!products || !products.length) {
     return <div>Loading...</div>;
   }
@@ -28,36 +27,57 @@ const AddReview = () => {
 
   const create = (ev) => {
     ev.preventDefault();
-    try{
-      dispatch(createReview({ userId: auth.id, productId: product.id, rating, title, content}));
+    try {
+      dispatch(
+        createReview({
+          userId: auth.id,
+          productId: product.id,
+          rating,
+          title,
+          content,
+        })
+      );
       setTitle('');
       setContent('');
       setRating(0);
       navigate(`/products/${product.id}`);
-    }
-    catch(ex){
+    } catch (ex) {
       console.log(ex);
     }
   };
-  
+
   return (
     <div>
-      <h2>Create a Review for: { product.name }</h2>
-      <form onSubmit={ create }>
+      <Typography variant='h4'>Create a Review for: {product.name}</Typography>
+      <form onSubmit={create}>
         {/*<input value={ rating } onChange={ ev => setRating(ev.target.value) } placeholder='rating' />*/}
-        <Typography component="legend">Rating</Typography>
+        <Typography component='legend'>Rating</Typography>
         <Rating
-          name="simple-controlled"
+          name='simple-controlled'
           value={rating}
-          onChange={ev => setRating(Number(ev.target.value))}
+          onChange={(ev) => setRating(Number(ev.target.value))}
         />
-        
-        <input value={ title } onChange={ ev => setTitle(ev.target.value)} placeholder='title'/>
-        <input value={ content } onChange={ ev => setContent(ev.target.value)} placeholder='written review'/>
-        <button>Add Review</button>
+
+        <TextField
+          required
+          label='Title'
+          margin='dense'
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+          placeholder='title'
+        />
+        <TextField
+          required
+          label='Your Comments'
+          margin='dense'
+          value={content}
+          onChange={(ev) => setContent(ev.target.value)}
+          placeholder='Written Review'
+        />
+        <Button type='submit'>Add Review</Button>
       </form>
     </div>
-    );
+  );
 };
 
 export default AddReview;
--- src/Components/Orders.js
+++ src/Components/Orders.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 const Orders = () => {
   const { orders, products } = useSelector((state) => state);
 
   return (
     <div>
-      <h1>Order History</h1>
-      <ul className="order-list">
+      <Typography variant='h4'>Order History</Typography>
+      <ul className='order-list'>
         {orders.map((order) => {
           return (
-            <li key={order.id} className="order-item">
-              <Link to={`/orders/${order.id}`} className="order-link">
+            <li
+              key={order.id}
+              className='order-item'
+            >
+              <Link
+                to={`/orders/${order.id}`}
+                className='order-link'
+              >
                 Order ID: {order.id}
               </Link>
               <br></br>
-              <span className="order-date">
+              <span className='order-date'>
                 Date: {order.createdAt.slice(0, 10)}
               </span>
               <br></br>
-              <span className="order-total">
+              <span className='order-total'>
                 Total: $
                 {order.lineItems.reduce((total, lineItem) => {
                   const product = products.find(
@@ -30,22 +37,25 @@ const Orders = () => {
                 }, 0)}
               </span>
               <br></br>
-              <ul className="line-item-list">
+              <ul className='line-item-list'>
                 {order.lineItems.map((lineItem) => {
                   const product = products.find(
                     (p) => p.id === lineItem.productId
                   );
                   return (
-                    <li key={lineItem.id} className="line-item">
-                      <span className="product-name">
+                    <li
+                      key={lineItem.id}
+                      className='line-item'
+                    >
+                      <span className='product-name'>
                         Product Name: {product.name}
                       </span>
                       <br></br>
-                      <span className="product-quantity">
+                      <span className='product-quantity'>
                         Quantity: {lineItem.quantity}
                       </span>
                       <br></br>
-                      <span className="product-price">
+                      <span className='product-price'>
                         Price: ${product.price}
                       </span>
                       <br></br>
